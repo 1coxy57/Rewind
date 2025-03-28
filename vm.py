18fceb2f447a178a579938fdb67bcd1b0099c594
coxy57 <158783816+1coxy57@users.noreply.github.com>
2025-03-28 12:44:11 -0400

    Create vm.py

---
+++ vm.py
@@ -0,0 +1,18 @@
+from typing import List, Dict, Callable, Any
+import random
+from bson import BSON
+from sys import exit
+import random
+
+'''
+Goal:
+🟢 Easy: build hash generator
+
+🟠 Medium: recode the bytecode logic so you are able to print in the file without the program exitting
+
+🔴 Hard: build hash generator for c0xy_hash
+
+created by coxy.57
+'''
+
+print('soon')
